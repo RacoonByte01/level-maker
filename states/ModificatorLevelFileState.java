0cf4982c7325b5e1d98632c12a634ed54e6c4ca9
JaviLeL <javier212002@gmail.com>
2024-04-27 18:44:19 +0200

    adding message error for name level empity

--- states/ModificatorLevelFileState.java
+++ states/ModificatorLevelFileState.java
@@ -9,6 +9,7 @@ import settings.Constants;
 import settings.Settings;
 import ui.Acttion;
 import ui.Button;
+import ui.MessageError;
 import ui.TextBox;
 
 /**
@@ -21,14 +22,16 @@ public class ModificatorLevelFileState extends State {
     LevelDTO level;
     private TextBox textBoxName;
     private Button buttonSend;
+    MessageError messageError;
 
     public ModificatorLevelFileState() {
         this(null);
     }
 
     public ModificatorLevelFileState(LevelDTO level) {
-        textBoxName = new TextBox(Settings.width / 2, Settings.height / 2, Settings.width * 2 / 3, 55,
-                "Nombre del nivel", 20);
+        textBoxName = new TextBox(Settings.width / 2, Settings.height * 2 / 6, Settings.width * 2 / 3, 50,
+                "Nombre del nivel *", 20);
+        messageError = new MessageError(Settings.width / 2, Settings.height * 3 / 6, 500, 50);
         if (level != null) {
             this.level = level;
             textBoxName.setText(level.getNombre());
@@ -36,20 +39,25 @@ public class ModificatorLevelFileState extends State {
             buttonSend = new Button(Settings.width / 2, Settings.height * 5 / 6, "Crear", new Acttion() {
                 @Override
                 public void accionARealizar() {
-                    new LevelCRUD().insert(new LevelDTO(textBoxName.getText(), null, SelectLevel.user.getCorreo()));
-                    @SuppressWarnings("unchecked")
-                    List<LevelDTO> levels = (List<LevelDTO>) new LevelCRUD().select(SelectLevel.user.getCorreo());
-                    State.setActualState(new SelectLevel(levels, SelectLevel.user));
+                    if (textBoxName.getText().length() == 0) {
+                        messageError.setText("Porfavor nombra al nivel");
+                        messageError.setVisibleTime(250);
+                    } else {
+                        new LevelCRUD().insert(new LevelDTO(textBoxName.getText(), null, SelectLevel.user.getCorreo()));
+                        @SuppressWarnings("unchecked")
+                        List<LevelDTO> levels = (List<LevelDTO>) new LevelCRUD().select(SelectLevel.user.getCorreo());
+                        State.setActualState(new SelectLevel(levels, SelectLevel.user));
+                    }
                 }
             });
         }
-
     }
 
     @Override
     public void update() {
         textBoxName.update();
         buttonSend.update();
+        messageError.update();
     }
 
     @Override
@@ -58,5 +66,6 @@ public class ModificatorLevelFileState extends State {
         g.fillRect(0, 0, Settings.width, Settings.height);
         textBoxName.draw(g);
         buttonSend.draw(g);
+        messageError.darw(g);
     }
 }
