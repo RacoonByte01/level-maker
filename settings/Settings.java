43fc3118293182492ac4dbf6cd819498d4081af1
JaviLeL <javier212002@gmail.com>
2025-06-17 02:39:00 +0200

    style: new order in the files

--- settings/Settings.java
+++
@@ -1,8 +0,0 @@
-package settings;
-
-public class Settings {
-    // Opciones de la ventana
-    public static int width = 1280, height = 720;
-    public static byte cellSize = 32;
-    public static boolean fullScreen = false;
-}
